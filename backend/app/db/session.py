85fb1eadf80c2d72b22eb9446cc5eef5dddd51a9
split73 <54321lexa12345@mail.ru>
2026-08-11 16:42:34 +0400

    vercel session db fix

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,14 +1,24 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session, sessionmaker
-
 from ..config import settings
 
-engine = create_engine(settings.database_url, pool_pre_ping=True)
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False, class_=Session)
+engine = None
+SessionLocal = None
 
+def get_engine():
+    global engine
+    if engine is None:
+        engine = create_engine(settings.database_url, pool_pre_ping=True)
+    return engine
+
+def get_session_local():
+    global SessionLocal
+    if SessionLocal is None:
+        SessionLocal = sessionmaker(bind=get_engine(), autoflush=False, autocommit=False, class_=Session)
+    return SessionLocal
 
 def get_db():
-    db = SessionLocal()
+    db = get_session_local()()
     try:
         yield db
     finally:
